4ea702cd28fde481649074704bcceb1d49260ca4
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-05-26 00:28:43 +0500

    23123

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === 'development') {
 // CORS configuration
 const allowedOrigins = process.env.NODE_ENV === 'production' 
   ? [process.env.FRONTEND_URL, 'http://localhost:5174', 'http://127.0.0.1:5174']
-  : ['http://localhost:5174', 'http://127.0.0.1:5174', 'http://localhost:5173', 'http://127.0.0.1:5173'];
+  : ['http://localhost:5174', 'http://127.0.0.1:5174', 'http://localhost:5173', 'https://apploo-1.vercel.app/', 'http://127.0.0.1:5173'];
 
 app.use(cors({
   origin: function(origin, callback) {
